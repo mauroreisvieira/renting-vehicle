13ae8d535402976fb8b0198e5e0e9710990f9e32
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2017-11-11 13:26:17 +0000

    - Updated webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: "./src/index.tsx",
+    entry : {
+        index: './src/index.tsx'
+    },
+
     output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: '[name].js',
+        publicPath: '/dist'
     },
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+            { // sass / scss loader for webpack
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: extractTextPlugin.extract({
+                    use : ['css-loader', 'sass-loader']
+                })
+            },
+        ],
+        loaders: [
+            { // sass / scss loader for webpack
+                test: /\.(sass|scss)$/,
+                exclude: /node_modules/,
+                loader: extractTextPlugin.extract(['css-loader', 'sass-loader'])
+            }
         ]
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"],
+        modules: ['src', 'node_modules']
     },
 
     // When importing a module whose path matches one of the following, just
@@ -31,4 +54,17 @@ module.exports = {
         "react": "React",
         "react-dom": "ReactDOM"
     },
+
+    plugins: [
+        new extractTextPlugin({ // define where to save the file
+            filename: __dirname + "/dist/css/main.css",
+            allChunks: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        new webpack.ProvidePlugin({
+            "React": "react",
+        }),
+    ],
 };
