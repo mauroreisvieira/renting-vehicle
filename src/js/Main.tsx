137c55b6b865386e05319a8dc752e088110b3222
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2017-11-16 00:00:49 +0000

    - Added scss in project.

--- src/js/Main.tsx
+++ src/js/Main.tsx
@@ -1,37 +1,37 @@
 import * as React from 'react';
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from 'react-dom';
 
-import Vehicle from "./model/Vehicle";
+import Vehicle from './model/Vehicle';
 
-import { HelloComponent } from "./components/HelloComponent";
-import {VehicleComponent} from "./components/VehicleComponent";
+import { HelloComponent } from './components/HelloComponent';
+import {VehicleComponent} from './components/VehicleComponent';
 
-import '../scss/index.scss';
+import '../scss/app.scss';
 
 export default class Main {
     public car: Vehicle;
     public motorcycle: Vehicle;
 
     constructor () {
-        this.car = new Vehicle("red", 10, 5, "Car");
-        this.motorcycle = new Vehicle("blue", 2, 2, "Motorcycle");
+        this.car = new Vehicle('red', 10, 5, 'Car');
+        this.motorcycle = new Vehicle('blue', 2, 2, 'Motorcycle');
         this.init();
     }
 
-    public init() : void {
+    public init(): void {
         ReactDOM.render(
-            <HelloComponent compiler="TypeScript" framework="React" />,
-            document.getElementById("hello")
+            <HelloComponent compiler='TypeScript' framework='React' />,
+            document.getElementById('hello')
         );
 
         ReactDOM.render(
             <VehicleComponent vehicleType={this.car.vehicleType} color={this.car.color} numWheels={this.car.numWheels} age={this.car.age} />,
-            document.getElementById("car")
+            document.getElementById('car')
         );
 
         ReactDOM.render(
             <VehicleComponent vehicleType={this.motorcycle.vehicleType} color={this.motorcycle.color} numWheels={this.motorcycle.numWheels} age={this.motorcycle.age} />,
-            document.getElementById("motorcycle")
+            document.getElementById('motorcycle')
         );
     }
 }
